9d7d0e198994fe2eda2dc24eb25bdd0fed5af13c
Matias <matiascschatz@gmail.com>
2022-01-25 20:44:43 -0300

    mejoras v.4

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,14 @@
 import React from "react";
 import Copyright from "./Copyright";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
 
 const Footer = () => {
+    const {formState: { errors }, register, handleSubmit} = useForm();
+
+    const enviarCorreo = ()=>{
+        toast.success("Se ha Enviado Nuestro Catalogo con exito a su Email.")
+    }
 
     return (
         <div className="footer">
@@ -9,18 +16,29 @@ const Footer = () => {
                 <div className="socialFooter">
                     <h4>Seguinos en Nuestras Redes</h4>
                     <ul>
-                        <li><ion-icon name="logo-instagram"></ion-icon></li>
-                        <li><ion-icon name="logo-facebook"></ion-icon></li> 
-                        <li><ion-icon name="logo-twitter"></ion-icon></li> 
-                        <li><ion-icon name="logo-whatsapp"></ion-icon></li> 
+                        <li><a href="https://www.instagram.com/?hl=es"><ion-icon name="logo-instagram"></ion-icon></a></li>
+                        <li><a href="https://www.facebook.com/"><ion-icon name="logo-facebook"></ion-icon></a></li> 
+                        <li><a href="https://twitter.com/home"><ion-icon name="logo-twitter"></ion-icon></a></li> 
+                        <li><a href="https://www.whatsapp.com/?lang=es"><ion-icon name="logo-whatsapp"></ion-icon></a></li> 
                     </ul>
                 </div>
 
                 <div className="contactFooter">
                     <h4>Déjanos tu Email para recibir Nuestro Catálogo y Ofertas</h4>
                     <div className="inputContactFooter">
-                        <input type="text" placeholder="Tu Email"/>
-                        <button>Enviar</button>
+                        <form onSubmit={handleSubmit(enviarCorreo)}>
+                            <input 
+                                type="text" 
+                                placeholder="Tu Email"
+                                {...register("EmailFooter",{
+                                    required: {value:true, message: "Por Favor, Ingrese Su Email"},
+                                    pattern: {value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, message: "Email Invalido"}
+                                })}/>
+
+                            <button>Enviar</button>
+                            
+                            <span className="errorInputFooter">{errors?.EmailFooter?.message}</span>
+                        </form>
                     </div>
                 </div>
             </footer>
